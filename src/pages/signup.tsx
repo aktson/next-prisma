c673f36d08f697fb701df6af66c7871780c7d819
Ankit Soni <79087448+aktson@users.noreply.github.com>
2023-04-17 10:42:08 +0200

    create user function and signup function working now

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -42,10 +42,14 @@ const SignUp: FC<SignUpProps> = (): JSX.Element => {
 		try {
 			const response = await fetch("http://localhost:3000/api/users", {
 				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
 				body: JSON.stringify(data),
 			});
 			const result = await response.json();
 			console.log(result);
+
 		} catch (error) {
 			console.log(error);
 		} finally {
