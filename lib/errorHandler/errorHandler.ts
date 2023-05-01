4d2aebfe15204e1e3e1ef34737c557f54603d824
Ankit Soni <akt_soni@yahoo.co.in>
2023-05-01 10:35:52 +0200

    Delete errorHandler.ts

--- lib/errorHandler/errorHandler.ts
+++
@@ -1,5 +0,0 @@
-export default function errorHandler(res: any, status: string, statusCode?: number, message?: string) {
-	return res
-		.status(statusCode)
-		.json({ status: status || "Feil", statusCode: res.statusCode || 500, message: message || "An unknown error occured" });
-}
